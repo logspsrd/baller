4784ef3ae54a4cc62f9f1214d6a428eeafcf3392
logspsrd <loganspessard@gmail.com>
2025-07-15 14:59:11 -0400

    changed entire community page ui

--- src/CommunityPage.jsx
+++ src/CommunityPage.jsx
@@ -3,10 +3,10 @@
 const CommunityPage = () => {
 
     return(
-        <>
-            <div className="h-screen w-[1600px] top-8 left-[50%] -translate-x-[50%] rounded-[50px] relative bg-gray-100">
-                <div className="h-[550px] w-[525px] flex flex-col absolute top-40 left-24">
-                    <h2 className="h-[48px] w-[600px] text-[hsl(0,_0%,0%)] font-bold text-[24px]">Explore different communities.</h2>
+        <div className="h-screen">
+            <div className="h-[800px] w-[1400px] top-16 left-[50%] -translate-x-[50%] rounded-[50px] relative bg-gray-100">
+                <div className="h-[350px] w-[450px] flex flex-col absolute top-40 left-24">
+                    <h2 className="h-[150px] w-[400px] flex text-[hsl(0,_0%,0%)] font-bold text-[48px]">Explore different communities.</h2>
                     <div className="h-[125px] w-[500px] pt-4 flex flex-wrap gap-2">
                         <div className="bg-white h-8 w-40 pr-1 gap-1 rounded-3xl flex items-center justify-center">
                             <box-icon className="h-6 w-6" color="gray" name='basketball'></box-icon>
@@ -30,26 +30,19 @@ const CommunityPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="h-[250px] w-[700px] flex items-center justify-center absolute left-[50px] top-[510px] bg-[url('./assets/sparring-boxing.jpg')] bg-cover bg-center overflow-hidden">
-                <h2 className="text-7xl font-black text-red-500 mix-blend-color-burn">GET BETTER.</h2>
-                </div>
-                <div className="h-[600px] w-[750px] bg-white absolute top-40 left-[800px] rounded-4xl flex items-center justify-center gap-8">
-                    <div className="h-[500px] w-[300px] flex flex-col items-center rounded-3xl">
-                        <div className="h-8 w-full">
-                            <h2 className="h-8 w-full text-[hsl(0,_0%,0%)] font-bold text-[20px]">Community</h2>
+                <div className="h-[600px] w-[700px] bg-white absolute top-24 left-[600px] rounded-4xl flex items-center justify-center gap-8">
+                    <div className="h-[450px] w-[300px] flex flex-col">
+                        <div className="h-[75px] w-full text-[24px] font-bold pt-4"><h2>Find and meet new people!</h2></div>
+                        <div className="h-[200px] w-full">
+                            <p className="text-[18px] font-semibold text-[hsl(0,_0%,_70%)]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi sint necessitatibus delectus aspernatur vitae minima facilis ullam incidunt.</p>
                         </div>
-                        <div className="h-[500px] w-[300px] ">
-                            <p className="font-regular text-[16px] text-[hsl(0,_0%,40%)] pt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto repellat asperiores quidem? Magni mollitia animi tenetur dolores perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium officia, sit commodi asperiores, iure expedita officiis eligendi iusto odit quibusdam, magnam illum? Molestias explicabo eum asperiores voluptas voluptatem repudiandae aut?</p>
-                        </div>
-                        <div className="h-[200px] w-[300px] flex flex-col relative mt-4">
-                            <h2 className="h-[50px] w-full text-[18px] relative  font-bold">Join the community</h2>
-                            <button className="bg-black font-regular text-[16px] text-white h-[48px] w-full rounded-full cursor-pointer hover:bg-[#262626]">Connect</button>
-                        </div>
+                        <h2 className="h-[100px] w-[275px] text-[24px] font-bold pt-12">Join to access all communities</h2>
+                        <button className="bg-black h-[50px] w-[300px] flex items-center pl-4 rounded-4xl text-white mt-8 hover:bg-[hsl(0,_0%,_20%)] transition-all duration-300 cursor-pointer">Join</button>
                     </div>
-                    <div className="bg-gray-100 h-[500px] w-[350px] rounded-[35px]"></div>
+                    <div className="h-[450px] w-[250px] bg-gray-100 rounded-4xl"></div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
